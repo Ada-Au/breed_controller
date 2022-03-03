dc47f9949dd79d0421b009403db4cb9fec6d9b1e
adaau0209 <adaau0209@gmail.com>
2022-03-04 01:43:03 +0800

    added joystick

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Joystick from "./components/joystick";
 
 export default function App() {
+  const [pos1, setPos1] = useState({ x: 0, y: 0 });
+  const [pos2, setPos2] = useState({ x: 0, y: 0 });
+
+  const handleChange1 = (pos) => {
+    setPos1(pos);
+  };
+  const handleChange2 = (pos) => {
+    setPos2(pos);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.joystick1}>
+        <Joystick padRadius={90} handleChange={handleChange1} />
+      </View>
+      <View style={styles.joystick2}>
+        <Joystick
+          padRadius={40}
+          thumbRadius={20}
+          handleChange={handleChange2}
+        />
+      </View>
+      <View style={styles.data}>
+        <Text>
+          Joystick1.x:{pos1.x.toPrecision(4)} Joystick1.y:
+          {pos1.y.toPrecision(4)}
+        </Text>
+        <Text>
+          Joystick2.x:{pos2.x.toPrecision(4)} Joystick2.y:
+          {pos2.y.toPrecision(4)}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +41,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  joystick1: {
+    flex: 0.3,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  joystick2: {
+    flex: 0.3,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  data: {
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/joystick.js
@@ -0,0 +1,121 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Animated, View, StyleSheet, PanResponder, Text } from "react-native";
+
+function Joystick({ padRadius = 64, thumbRadius = 30, handleChange }) {
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const center = {
+    x: padRadius - thumbRadius - 3,
+    y: padRadius - thumbRadius - 3,
+  };
+
+  useEffect(() => {
+    handleChange({
+      x: pos.x / (padRadius - thumbRadius / 2),
+      y: -pos.y / (padRadius - thumbRadius / 2),
+    });
+  }, [pos]);
+
+  const pan = useRef(new Animated.ValueXY(center)).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.flattenOffset();
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        });
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        let angle = Math.atan2(
+          gestureState.moveY - gestureState.y0,
+          gestureState.moveX - gestureState.x0
+        );
+        let distance = Math.min(
+          padRadius - thumbRadius / 2,
+          Math.sqrt(
+            Math.pow(gestureState.moveX - gestureState.x0, 2) +
+              Math.pow(gestureState.moveY - gestureState.y0, 2)
+          )
+        );
+        setPos({
+          x: Math.cos(angle) * distance,
+          y: Math.sin(angle) * distance,
+        });
+
+        return Animated.event([null, { dx: pan.x, dy: pan.y }], {
+          useNativeDriver: false,
+        })(evt, gestureState);
+      },
+      onPanResponderRelease: () => {
+        setPos({ x: 0, y: 0 });
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false,
+        }).start();
+      },
+    })
+  ).current;
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          width: padRadius * 2,
+          borderRadius: padRadius,
+        },
+      ]}
+    >
+      <View
+        style={[
+          {
+            top: pos.y + padRadius - thumbRadius - 3,
+            left: pos.x + padRadius - thumbRadius - 3,
+            width: thumbRadius * 2,
+            borderRadius: thumbRadius,
+            color: "black",
+          },
+          styles.thumb,
+        ]}
+      />
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }],
+        }}
+        {...panResponder.panHandlers}
+      >
+        <View
+          style={{
+            aspectRatio: 1,
+            position: "absolute",
+            width: thumbRadius * 2,
+            borderRadius: thumbRadius,
+          }}
+        />
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    shadowColor: "black",
+    shadowRadius: 2,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+
+    position: "absolute",
+    aspectRatio: 1,
+    borderWidth: 3,
+    borderColor: "rgba(255,255,255,0.5)",
+    backgroundColor: "rgba(255,255,255,0.1)",
+  },
+  thumb: {
+    position: "absolute",
+    backgroundColor: "white",
+    aspectRatio: 1,
+  },
+});
+
+export default Joystick;
